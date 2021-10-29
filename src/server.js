84ef7a31c6eebd7007338dde0a9b9902d7f822e1
heekang2271 <gmlrkd3922@gmail.com>
2021-10-29 20:46:25 +0900

    node js setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = 8000;
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
